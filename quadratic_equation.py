0d7eccd03d9f568d912bb90f09d85276d1d346ef
eremeevfd <dartfedor@mail.ru>
2016-12-15 08:59:49 +0300

    1

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -2,7 +2,6 @@ from math import sqrt
 
 
 def get_roots(a, b, c):
-    # lol
     discriminant = b ** 2 - 4 * a * c
     root1 = (-b - sqrt(discriminant)) / (2 * a)
     root2 = (-b + sqrt(discriminant)) / (2 * a)
